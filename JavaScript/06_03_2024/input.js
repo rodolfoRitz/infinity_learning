6aa4ed199645b92b215b098f9174f4b5c9a30c00
Rodolfo Ritz <rodolfo.ritza@gmail.com>
2024-03-06 18:42:50 -0300

    Add files via upload

---
+++ JavaScript/06_03_2024/input.js
@@ -0,0 +1,15 @@
+// import datetime
+
+// let idade = Number(prompt('Digite sua idade (em anos): '))
+let raio = Number(prompt('Digite o RAIO do seu círculo: '))
+
+// let ano_nasc = 2024 - idade
+let area_circ = raio * raio
+
+// console.log('O ano que você nasceu é: ', ano_nasc)
+console.log('A área do círculo a partir do raio informado é: ', area_circ)
+
+// Crie um programa que solicite ao usuário que insira o raio de um círculo
+// usando o prompt e, em seguida, calcule e exiba a área do círculo usando o
+// console.
+
---
+++ JavaScript/06_03_2024/script.js
@@ -0,0 +1,14 @@
+var batata = 'batata'
+var n1 = 10
+var n2 = 3.14
+var eCachorro = true
+var aluno1
+var aluno2 = null
+
+console.log(typeof(batata))
+console.log(typeof(n1))
+console.log(typeof(n2))
+console.log(typeof(eCachorro))
+console.log(typeof(aluno1))
+console.log(typeof(batata))
+console.log(typeof(aluno2))
