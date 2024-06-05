2f5fc0126a4d4a119435a164bf9227b880fd0ac6
Rodolfo Ritz <rodolfo.ritza@gmail.com>
2024-06-05 18:38:41 -0300

    Add files via upload
    https://messy-button-d76.notion.site/Aula-13-Try-e-Catch-a9e87c1f21eb4cf0b6fea508ef777106

---
+++ JavaScript/05_06_2024/script.js
@@ -0,0 +1,30 @@
+let escolhaUsuario;
+let botaoEnviar;
+
+botaoEnviar.addEventListener("click", () => {
+  debugger;
+  try {
+    let usuario = escolhaUsuario.value;
+    usuario.toLowerCase();
+    console.log("Consegui Converter");
+    console.log(usuario);
+  } catch (erro) {
+    console.log(erro);
+  } finally {
+    console.log("A tentativa de conversão foi concluída");
+  }
+});
+
+// 1. Crie um bloco `try` que tenta executar um código que define uma
+//    variável `x=10`. Adicione um bloco `catch` para capturar e exibir
+//    erros no console.
+try {
+  let x = 10;
+} catch (erro) {
+  console.log("Consegui Converter");
+  console.log(erro);
+}
+
+// 2. Modifique o exercício anterior para forçar um erro colocando para
+//    imprimir no console uma variável não definida e verifique se o `catch`
+//    captura o erro.
